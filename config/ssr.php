179422f3b1fbd41c0ac55a60a17b3fad14b0bfa4
Abu Said Sheikh <30535762+abusaidskbabu@users.noreply.github.com>
2023-01-22 17:38:12 +0600

    ssr

--- config/ssr.php
+++ config/ssr.php
@@ -31,7 +31,7 @@ return [
      * Extra setup for the Node engine.
      */
     'node' => [
-        'node_path' => env('NODE_PATH', '/home/kholabazaar/nodevenv/nodejs/16/bin/node'),
+        'node_path' => env('NODE_PATH', '/home/techhutc/nodevenv/nodejs/12/bin/node'),
         // 'node_path' => env('NODE_PATH','D:\xampp\nodejs\node.exe'),
         'temp_path' => storage_path('app/ssr'),
     ],
